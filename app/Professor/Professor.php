396ba92bbe2664e1513614bdb269379d62566a65
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-02-28 23:52:29 -0600

    Create the Professor model

--- app/Professor/Professor.php
+++ app/Professor/Professor.php
@@ -3,8 +3,49 @@
 namespace App\Professor;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Professor extends Model
-{
-    //
+{	
+	use SoftDeletes;
+
+	/**
+	 * The attributes that are mass assignable
+	 * @var array
+	 */
+    protected $fillable = [
+    	'firstname',
+    	'lastname',
+    	'school_id',
+    	'department_id',
+    	'directory_url',
+    	'approved',
+   	];
+
+   	/**
+   	 * The attributes that should be mutated to dates
+   	 * @var array
+   	 */
+   	protected $dates = [
+   		'created_at',
+   		'updated_at',
+   		'deleted_at',
+   	];
+
+   	/**
+   	 * The attributes that should be mutated to native types
+   	 * @var array
+   	 */
+   	protected $casts = [
+   		'approved' => 'boolean',
+   	];
+
+   	/**
+   	 * Get the full name of the professor
+   	 * @return string 
+   	 */
+   	public function getNameAttribute()
+   	{
+   		return $this->firstname . ' ' . $this->lastname;
+   	}
 }
