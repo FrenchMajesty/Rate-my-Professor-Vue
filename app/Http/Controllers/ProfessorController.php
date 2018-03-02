5fe320d79b59a66e11bf8d5c72b745163d75e679
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-01 23:24:11 -0600

    Create method to delete and update a professor

--- app/Http/Controllers/ProfessorController.php
+++ app/Http/Controllers/ProfessorController.php
@@ -32,9 +32,19 @@ class ProfessorController extends LaravelController
     }
 
     /**
+     * Fetch and return a professor entry
+     * @param  \App\Professor\Professor $prof Professor
+     * @return \App\Professor\Professor               
+     */
+    public function index(Professor $prof)
+    {
+        return $prof;
+    }
+
+    /**
      * Handle the request to create a new professor
      * @param  \Illuminate\Http\Request $request Request
-     * @return \Illuminate\Http\Response           
+     * @return void          
      */
     public function create(Request $request)
     {
@@ -55,11 +65,38 @@ class ProfessorController extends LaravelController
         ]);
     }
 
+    /**
+     * Handle the request to update a professor's information
+     * @param  App\Professor\Professor $prof    Professor
+     * @param  \Illuminate\Http\Request $request Request
+     * @return void            
+     */
     public function update(Professor $prof, Request $request)
     {
-    	$this->validate($request, [
-    		'firstname' => 'required|string',
-    	]);
+    	Validator::make($request->all() , [ 
+            'firstname' => 'required|alpha_dash|max:25',
+            'lastname' => 'required|alpha_dash|max:30',
+            'school_id' => 'required|numeric|exists:schools,id',
+            'department_id' => 'required|numeric|exists:departments,id',
+            'directory_url' => 'nullable|url',
+        ])->validate();
+
+        $prof->firstname = $request->firstname;
+        $prof->lastname = $request->lastname;
+        $prof->school_id = $request->school_id;
+        $prof->department_id = $request->department_id;
+        $prof->directory_url = $request->directory_url;
+        $prof->save();
+    }
+
+    /**
+     * Delete a professor entry
+     * @param  App\Professor\Professor $prof Professor
+     * @return void          
+     */
+    public function delete(Professor $prof)
+    {
+        $prof->delete();
     }
 
     protected function filterSchool(Builder $query, $method, $clauseOperator, $value, $in)
