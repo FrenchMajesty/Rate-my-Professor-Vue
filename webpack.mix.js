272a0923acfa5b8da9104a48f2a9ea04252409ad
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-02 23:35:16 -0600

    Set up webpack aliases
    To make it easier to load modules.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,12 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .webpackConfig({
+   		resolve: {
+   			alias: {
+   				Js: path.resolve(__dirname, 'resources/assets/js/'),
+   				Mixins: path.resolve(__dirname, 'resources/assets/mixins/'),
+   			}
+   		}
+   });
