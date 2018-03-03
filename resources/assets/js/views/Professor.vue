d54b98d8b962e07fd5ec7cae53cf64161f1771c8
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-03 02:30:43 -0600

    Create Professor view

---
+++ resources/assets/js/views/Professor.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<Navbar :user="shared.user" :searchData="searchData"></Navbar>
+		<div class="row space-above">
+			<div class="col-md-4">
+				<QuickSearch :searchData="searchData" />
+			</div>
+			<div class="col-md-8">
+				Synopsis here son..
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Navbar from '../components/common/Navbar';
+	import QuickSearch from '../components/QuickSearch';
+	import {user} from '../shared';
+
+	export default {
+		name: 'Professor',
+		components: {
+			Navbar, QuickSearch
+		},
+		data() {
+			return {
+				searchData: [],
+				shared: {
+					user,
+				}
+			}
+		},
+	};
+</script>
+
+<style scoped>
+	.space-above {
+		padding-top: 5em;
+	}
+</style>
