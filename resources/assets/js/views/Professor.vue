40f3be959f090a34116b41c64822eb8f6ae50c2d
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-04 20:10:26 -0600

    Update Professor view to add profile section
    I added the professor details card as well as the reviews container.

--- resources/assets/js/views/Professor.vue
+++ resources/assets/js/views/Professor.vue
@@ -1,25 +1,75 @@
 <template>
 	<ProfileContainer :user="shared.user" :searchData="searchData">
-		The profile goes here mate
+		<ProfessorDetailsCard :prof="prof" :similarProfs="[]"></ProfessorDetailsCard>
+		
+		<p v-if="prof.ratings.length > 0" class="md-title">Student Reviews</p>
+		<div class="md-layout md-gutter" v-if="prof.ratings.length > 0">
+			<ProfessorReview
+				size="md-size-80"
+				v-for="rating in prof.ratings"
+				:key="rating.id"
+				:rating="rating"
+			></ProfessorReview>
+		</div>
+		<div v-else>
+			<md-empty-state
+	      md-icon="info_outline"
+	      md-label="No reviews for the moment"
+	      md-description="There seems to be no review available for this professor.. Why not be the first to write one?">
+	      <md-button class="md-primary md-raised">Write a review</md-button>
+	    </md-empty-state>
+		</div>
 	</ProfileContainer>
 </template>
 
 <script>
 	import ProfileContainer from '../components/ProfileContainer';
+	import ProfessorDetailsCard from '../components/ProfessorDetailsCard';
+	import ProfessorReview from '../components/ProfessorReview';
 	import {user} from '../shared';
 
 	export default {
 		name: 'Professor',
 		components: {
-			ProfileContainer,
+			ProfileContainer, ProfessorDetailsCard, ProfessorReview,
 		},
 		data() {
 			return {
 				searchData: [],
+				prof: {
+					name: 'John Doe',
+					department: {
+						id: 9,
+						name: 'Science',
+					},
+					school: {
+						id: 99,
+						name: 'Harvard',
+						location: 'College Station,TX',
+					},
+					avgRatings: {
+						overall: 2.3,
+						difficulty: 4.5,
+					},
+					ratings: [
+						{
+							id: 1,
+							overall_rating: 2.4,
+							difficulty_rating: 3.0,
+							comment: 'I dont even like eating ice cream!!',
+						},
+					],
+				},
 				shared: {
 					user,
 				}
-			}
+			};
 		},
 	};
 </script>
+
+<style scoped>
+	.md-title {
+		margin: 2em 0;
+	}
+</style>
