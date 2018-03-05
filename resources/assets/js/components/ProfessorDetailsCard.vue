d926e48692457bcc2dcbb9042973636e0cd4a4d9
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-04 20:09:24 -0600

    Create ProfessorDetailsCard Component

---
+++ resources/assets/js/components/ProfessorDetailsCard.vue
@@ -0,0 +1,123 @@
+<template>
+	<md-card class="md-layout padding-above">
+		<!-- Ratings container -->
+      	<md-content class="md-layout-item md-layout text-center">
+      	
+	      	<md-content class="md-layout-item md-size-100 smiley-container">
+	      		<md-icon :class="'md-size-4x '+overallRankingScale">tag_faces</md-icon>
+	      	</md-content>
+	      	<table class="md-layout-item md-size-80">
+	      		<tr class="md-layout">
+	      			<td :class="`${scoreNumberClass} ${overallRankingScale}`">{{avgOverall}}</td>
+	      			<td :class="scoreTextClass">Average Overall</td>
+	      		</tr>
+	      		<br/>
+	      		<tr class="md-layout">
+	      			<td :class="`${scoreNumberClass} ${difficultyRankingScale}`">{{avgDifficulty}}</td> 
+	      			<td :class="scoreTextClass">Average Difficulty</td>
+	      		</tr>
+	      	</table>
+     	</md-content>
+
+      <!-- Prof info container -->
+      <md-content class="md-layout-item">
+      	<h1 class="prof-name">{{prof.name}}</h1>
+
+      	<p class="md-subtitle">Professor of {{departmentName}} at <a href="#">{{prof.school.name}}</a>, {{prof.school.location}}.</p>
+      	<div>
+      		<a href="#">Submit a correction</a>
+      	</div>
+      </md-content>
+
+      <!-- School info container -->
+      <md-content class="md-layout-item">
+      	<h1>{{prof.school.name}}</h1>
+      	<p>Located at {{prof.school.location}}</p>
+      	<a href="#">Check out 2 other professors from this school</a>.
+      	<md-divider class="divider"></md-divider>
+      	<p>Other professors in the deparment of {{departmentName}}.</p>
+
+      	<p class="caption">This professor seems to be the only one in this department on the website.</p>
+      </md-content>
+    </md-card>
+</template>
+
+<script>
+	export default {
+		name: 'ProfessorDetailsCard',
+		props: {
+			prof: {
+				type: Object,
+				required: true,
+			},
+			similarProfs: {
+				type: Array,
+				required: true,
+			},
+		},
+		data() {
+			return {
+				scoreNumberClass: 'score md-layout-item md-size-50',
+				scoreTextClass: 'score-text md-layout-item md-size-50',
+			};
+		},
+		computed: {
+			departmentName() {
+				return this.prof.department.name.toLowerCase();
+			},
+			avgOverall() {
+				return 3.8;
+			},
+			avgDifficulty() {
+				return 4.6;
+			},
+			overallRankingScale() {
+				const {avgOverall: overall} = this;
+
+				if(overall >= 3.8) {
+					return 'text-success';
+				}else if(overall > 2.8) {
+					return 'text-warning';
+				}else {
+					return 'text-danger';
+				}
+			},
+			difficultyRankingScale() {
+				const {avgDifficulty: difficulty} = this;
+
+				if(difficulty >= 3.8) {
+					return 'text-danger';
+				}else if(difficulty > 2.7) {
+					return 'text-warning';
+				}else {
+					return 'text-success';
+				}
+			},
+		},
+	};
+</script>
+
+<style scoped>
+	.padding-above {
+		padding: 2em 0;
+	}
+	.score-text {
+		font-size: 1.5em;
+	}
+	.score {
+		font-size: 2.7em;
+	}
+	.smiley-container {
+		margin-bottom: 2em;
+	}
+	.prof-name {
+		margin-bottom: 1em;
+	}
+	.divider {
+		margin: 2em 0;
+	}
+	.caption {
+		opacity: 0.65;
+		font-style: italic;
+	}
+</style>
