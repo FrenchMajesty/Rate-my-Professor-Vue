b1391d27708e64111280938133af85ac48e5cbdb
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-08 23:00:52 -0600

    Create Teacher and Student RegistrationCard Components
    I broke these off out of the Register view into their own components because they would have otherwise made the Register vue component very crowded.

---
+++ resources/assets/js/components/Auth/TeacherRegistrationCard.vue
@@ -0,0 +1,87 @@
+<template>
+	<md-card class="md-layout-item md-size-40 md-layout md-gutter md-alignment-top-space-around" md-with-hover>
+		<form class="md-layout-item md-size-90" @submit.preventDefault="submitForm">
+			<md-card-header>
+				<h1 class="md-title">Sign up today!</h1>
+				<span class="md-subhead">Be among the teachers who are leading the movement toward a more transparent and accessible education model.</span>
+			</md-card-header>
+			<md-field>
+				<md-icon>person</md-icon>
+				<label>Your first name:</label>
+				<md-input
+					v-model="firstname"
+					placeholder="Please enter your first name here."
+					maxlength="50"
+				></md-input>
+			</md-field>
+
+			<md-field>
+				<md-icon>person</md-icon>
+				<label>Your last name:</label>
+				<md-input
+					v-model="lastname"
+					placeholder="Please enter your last name here."
+					maxlength="50"
+				></md-input>
+			</md-field>
+
+			<md-field>
+				<md-icon>email</md-icon>
+				<label>Your Email:</label>
+				<md-input
+					v-model="email"
+					placeholder="Please enter your email address here."
+				></md-input>
+			</md-field>
+
+			<md-field>
+				<md-icon>lock</md-icon>
+				<label>Your password:</label>
+				<md-input
+					type="password"
+					v-model="password"
+					placeholder="Enter a password at least 6 characters long."
+				></md-input>
+			</md-field>
+
+			<md-checkbox 
+				v-model="terms"
+			>
+				By clicking this box you agree to the <a href="#">Terms and Conditions of Use</a> as well as our <a href="#">Privacy Policy</a>.
+			</md-checkbox>
+
+			<md-card-actions>
+			<md-button class="md-primary md-raised space-below" :disabled="!formIsComplete" type="submit">Create an Account</md-button>	
+			</md-card-actions>
+		</form>
+	</md-card>
+</template>
+
+<script>
+	export default {
+		name: 'TeacherRegistrationCard',
+		data() {
+			return {
+				firstname: '',
+				lastname: '',
+				email: '',
+				password: '',
+				terms: false,
+			};
+		},
+		computed: {
+			formIsComplete() {
+				return this.firstname.length > 0 &&
+						this.lastname.length > 0 &&
+						this.email.length > 0 &&
+						this.password.length > 0 &&
+						this.terms == true;
+			}
+		},
+		methods: {
+			submitForm() {
+				console.log('registration form for teacher was submitted ');
+			}
+		},
+	};
+</script>
