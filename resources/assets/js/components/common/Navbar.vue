46f9e6ae338ac94fdf6a8a6055739d528c7a615e
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-18 21:11:03 -0500

    Add a loading spinner on top of search bar in Navbar Component
    Since data and such are going to be pulled from the api I need to have a loading spinner to indicate to users what is happening.
    
    Because of that, I needed to move up the data fetching from the NavbarSearchForm component to this one so I can control what to render and when.
    
    PS: I've added the documentation as a bonus because it was hurting my eyes to figure what was happening and procrastinating to 'document everything later' is a trap.

--- resources/assets/js/components/common/Navbar.vue
+++ resources/assets/js/components/common/Navbar.vue
@@ -6,8 +6,24 @@
 	  <b-collapse is-nav id="nav_collapse">
 
 	  		<!-- Search input -->
-			<NavbarSearchForm></NavbarSearchForm>
-
+	  		<transition
+	  			name="search"
+	  			mode="out-in"
+	  			enter-active-class="animated bounceIn"
+	  			leave-active-class="animated bounceOut"
+	  		>
+				<NavbarSearchForm 
+					v-if="! dataIsLoading && canShowSearchBar"
+					:profData="this.$store.state.prof.data"
+				></NavbarSearchForm>
+				<div v-else class="mr-auto ml-auto text-center">
+	    			<md-progress-spinner 
+	    				:md-diameter="50"
+	    				:md-stroke="5"
+	    				md-mode="indeterminate"
+	    			></md-progress-spinner>
+				</div>
+			</transition>
 	    <b-navbar-nav>
 	    	<md-button :to="{name: 'index'}" class="no-margin">Home</md-button>
 	    	<md-button v-if="isAdmin" href="#" class="no-margin">Admin Panel</md-button>
@@ -32,6 +48,7 @@
 </template>
 
 <script>
+	import { loadAllProfessorsData } from 'Js/store/api';
 	import NavbarSearchForm from './NavbarSearchForm';
 
 	export default {
@@ -40,20 +57,83 @@
 			NavbarSearchForm,
 		},
 		props: {
+			/**
+			 * The user's object model
+			 * @type {Object}
+			 */
 			user: {
 				type: Object,
 			},
 		},
+		/**
+		 * Wait 1 sec before allowing the search bar to show to avoid useless transitions
+		 */
+		mounted() {
+			setTimeout(() => this.canShowSearchBar = true, 1000);
+		},
+		/**
+		 * Verify if the search data is being loaded and if not, call fetchData() method
+		 */
+		created() {
+			const {data, beingFetched} = this.$store.state.prof;
+			if(!data && !beingFetched) {
+				this.fetchData();
+			}
+		},
+		data() {
+			return {
+				/**
+				 * Render condition of the search bar
+				 * @type {Boolean}
+				 */
+				canShowSearchBar: false,
+			};
+		},
 		computed: {
+			/**
+			 * Determine if the user is logged in
+			 * @return {Boolean} 
+			 */
 			isLoggedIn() {
 				return Boolean(this.user);
 			},
+			/**
+			 * Determine if the user is an administrator
+			 * @return {Boolean} 
+			 */
 			isAdmin() {
 				return this.user && this.user.rank > 3;
 			},
+			/**
+			 * Determine whether the screen is small (< 775px)
+			 * @return {Boolean} 
+			 */
 			smallScreenSize(){
 				return window.innerWidth < 775;
 			},
+			/**
+			 * Current state of the search data fetching request
+			 * @return {Boolean} 
+			 */
+			dataIsLoading() {
+				return this.$store.state.prof.beingFetched;
+			},
+		},
+		methods: {
+			/**
+			 * Fetch all the professors's data from the API and commit the action
+			 */
+			fetchData() {
+				this.$store.commit('updateProfsDataFetchingStatus', true);
+
+				loadAllProfessorsData().then(res => {
+					this.$store.commit({
+						type: 'updateProfsData',
+						data: res.data.profs,
+					});
+				})
+				.catch(error => console.log(error));
+			}
 		},
 	};
 </script>
