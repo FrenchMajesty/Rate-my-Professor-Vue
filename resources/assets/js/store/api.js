dbeb13b679f8b1422cb10233384084ceb267e830
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-03-30 00:08:30 -0500

    Add API method to fetch schools data

--- resources/assets/js/store/api.js
+++ resources/assets/js/store/api.js
@@ -6,6 +6,10 @@ export function loadAllProfessorsData() {
 	return axios.get(`${BASE_URL}/prof/fetch`);
 }
 
+export function loadAllSchoolsData() {
+	return axios.get(`${BASE_URL}/school/fetch`);
+}
+
 export function submitStudentRegistration(data) {
 	return axios.post(`${BASE_URL}/signup/student`, data);
 }
