9e7f4216d2818b3adaa820c66627e896e7b6505a
Verdi.K <Verdi@verdiks-air-2.all.disttccd.net>
2018-03-31 18:57:52 -0500

    Add reviews branch to the app's store
    So that I can start creating the process of creating reviews, voting on them and interacting with them.

--- resources/assets/js/store/store.js
+++ resources/assets/js/store/store.js
@@ -4,6 +4,7 @@ import VuexPersist from 'vuex-persist';
 import auth from './sub/auth';
 import schools from './sub/schools';
 import profs from './sub/profs';
+import reviews from './sub/reviews';
 
 const vuexLocalStorage = new VuexPersist({
 	key: 'vuex', 
@@ -20,16 +21,19 @@ export default new Vuex.Store({
 		...auth.state,
 		...profs.state,
 		...schools.state,
+		...reviews.state,
 	},
 	getters: {
 		...auth.getters,
 		...profs.getters,
 		...schools.getters,
+		...reviews.getters,
 	},
 	mutations: {
 		...auth.mutations,
 		...profs.mutations,
 		...schools.mutations,
+		...reviews.mutations,
 	},
 	plugins: [vuexLocalStorage.plugin],
 });
---
+++ resources/assets/js/store/sub/reviews.js
@@ -0,0 +1,57 @@
+const reviews = {
+	state: {
+		reviews: {
+			quick: null,
+			type: null,
+			inView: [],
+		},
+	},
+	getters: {
+		/**
+		 * Return the value of quick written review
+		 * @param  {Object} state The app's state
+		 * @return {String|Null}       
+		 */
+		quickReview(state) {
+			return state.reviews.quick;
+		},
+	},
+	mutations: {
+		/**
+		 * Update the quick review's value
+		 * @param  {Object} state          The app's state
+		 * @param  {String} review The quick review that was written
+		 * @return {Void}                
+		 */
+		writeQuickReview(state, review) {
+			state.reviews.quick = review;
+		},
+
+		/**
+		 * Update the reviews that are currently in view on screen
+		 * @param  {Object} state           The app's state
+		 * @param  {Array} payload.reviews The reviews to show
+		 * @param {String} payload.type The type of reviews they are
+		 * @return {Void}                 
+		 */
+		updateTheReviewsInView(state, {reviews, type}) {
+			state.reviews.inView = reviews;
+			state.reviews.type = type;
+		},
+
+		/**
+		 * Update the state to cast a vote on a review
+		 * @param  {Object} state        The app's state
+		 * @param  {Number} payload.voteType The type of vote that is being casted
+		 * @param  {Number} payload.id   The ID of the review to rate
+		 * @return {Void}              
+		 */
+		voteOnReview(state, {voteType, id}) {
+			const review = state.reviews.inView.find(review => review.id == id);
+
+			// ...
+		}
+	}
+};
+
+export default reviews;
