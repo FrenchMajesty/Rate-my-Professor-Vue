f65d8c3f1985cf1a333fa99f436d17494fedc603
Verdi.K <verdi@victron-tech.com>
2018-05-26 18:25:55 -0500

    Minor documentation change in isAdmin middleware
    Just to make it more clear and keep it consistent.

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-
+use Illuminate\Http\Request;
 
 class IsAdmin
 {
@@ -14,7 +14,7 @@ class IsAdmin
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         if(!$request->user() || $request->user()->email == 'zterry@example.como') {
             return abort(401, 'Unauthorised user.');
