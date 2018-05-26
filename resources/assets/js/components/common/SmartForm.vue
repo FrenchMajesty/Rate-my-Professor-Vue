7ec84aa597a8937e391bd6689accdcc9b2224107
Verdi.K <verdi@victron-tech.com>
2018-05-26 18:22:58 -0500

    Create the SmartForm component
    I caught myself having to copy/paste the clearFieldsErrors with a keydown event binder with every one of my forms which isn't effecient so I just created this simple wrapper for my forms so that I don't have to do it everywhere.

---
+++ resources/assets/js/components/common/SmartForm.vue
@@ -0,0 +1,43 @@
+<template>
+	<form 
+		@submit.preventDefault="submit"
+		@keydown="clearFieldErrors"
+	>
+		<slot></slot>
+	</form>
+</template>
+
+<script>
+	export default {
+		name: 'SmartForm',
+		props: {
+			/**
+			 * The form object of the form to submit
+			 * @type {Object}
+			 */
+			form: {
+				type: Object,
+				required: true,
+			},
+
+			/**
+			 * The method to trigger on submit
+			 * @type {Object}
+			 */
+			submit: {
+				type: Function,
+				required: true,
+			},
+		},
+		methods: {
+			/**
+			 * On keyDown, clear the errors on the target input
+			 * @param  {HTMLElement} event.target The target input's HTML element
+			 * @return {Void}                 
+			 */
+			clearFieldErrors({target}) {
+				this.form.errors.clear(target.getAttribute('data-name'));
+			},
+		},
+	}
+</script>
