005bcb8405a59900e2279d8bc77b0817858831c9
Verdi.K <verdi@victron-tech.com>
2018-05-27 02:06:59 -0500

    Add soft deletes to Department model
    No semi-important data is allowed to be deleted permanantly.
    
    While I was at it I also modified the Department model to support slugs.

--- app/Models/School/Department.php
+++ app/Models/School/Department.php
@@ -3,9 +3,12 @@
 namespace App\Models\School;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Department extends Model
 {
+    use SoftDeletes;
+
 	/**
 	 * The attributes that are mass assignable
 	 * @var array
@@ -21,6 +24,29 @@ class Department extends Model
      */
     protected $dates = [
     	'created_at',
-    	'updated_at',
+        'updated_at',
+    	'deleted_at',
     ];
+
+    /**
+     * Update the slug when saving the model
+     * @return void 
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($model) {
+            $model->slug = $model->id.'-'.str_slug($model->name);
+        });
+    }
+
+    /**
+     * Change the column with which to bind the model route
+     * @return string 
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
--- database/migrations/2018_03_01_060344_create_departments_table.php
+++ database/migrations/2018_03_01_060344_create_departments_table.php
@@ -18,6 +18,7 @@ class CreateDepartmentsTable extends Migration
             $table->string('name');
             $table->string('slug');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
