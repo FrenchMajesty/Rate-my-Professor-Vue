233c1543b03609b016a4e3a4c417a0342d29ee06
Verdi.K <verdi@victron-tech.com>
2018-05-27 02:07:33 -0500

    Create UserController@update method
    So that users would be able to update their profiles.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
-{
+{   
+    /**
+     * Handle the request to authenticate an user
+     * @param  \Illuminate\Http\Request $request Request
+     * @return array          
+     */
     public function login(Request $request)
     {
     	$this->validate($request, [
@@ -23,5 +29,36 @@ class UserController extends Controller
     	]);
 
     	return $response->getBody();
+    }
+
+    /**
+     * Handle a request to update a user's account information after validation
+     * @param  \Illuminate\Http\Request $request Request
+     * @param \App\User $user The user instance injected
+     * @return mixed         
+     */
+    public function update(Request $request, User $user)
+    {
+        $this->validate($request, [
+            'firstname' => 'required|alpha_dash|max:255',
+            'lastname' => 'required|alpha_dash|max:255',
+            'email' => 'required|email',
+        ]);
+
+        $isEmailTaken = User::where('email', $request->email)
+            ->whereNotIn('id', [$user->id])
+            ->first();
+
+        if($isEmailTaken) {
+            $errors = ['email' => ['This email is already used.']];
+            return response()->json(compact('errors'), 422);
+        }
+
+        $user->firstname = $request->firstname;
+        $user->lastname = $request->lastname;
+        $user->email = $request->email;
+        $user->save();
+        
+        return $user;
     }
 }
