217552c78e79b65f0fdaebbf7e0f74854f7c49d5
Verdi.K <verdi@victron-tech.com>
2018-05-27 18:16:29 -0500

    Add `SoftDeletes` trait to ProfessorReview model
    I had forgotten to add it when I created it.

--- app/Models/Professor/ProfessorReview.php
+++ app/Models/Professor/ProfessorReview.php
@@ -3,9 +3,12 @@
 namespace App\Models\Professor;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProfessorReview extends Model
 {
+    use SoftDeletes;
+    
 	/**
      * The attributes that are mass assignable.
      *
