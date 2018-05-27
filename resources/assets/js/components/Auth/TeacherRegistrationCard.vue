3504287c576672ef8ab5d587dc5ef400e0756637
Verdi.K <verdi@victron-tech.com>
2018-05-26 23:40:33 -0500

    Update TeacherRegistrationCard component
    To be closer to a real actual form.

--- resources/assets/js/components/Auth/TeacherRegistrationCard.vue
+++ resources/assets/js/components/Auth/TeacherRegistrationCard.vue
@@ -1,88 +1,141 @@
 <template>
 	<md-card md-with-hover>
-		<form class="md-layout-item md-size-90" @submit.preventDefault="submitForm">
+		<SmartForm class="md-layout-item md-size-90" :submit="submitForm" :form="form">
 			<md-card-header>
 				<h1 class="md-title">Sign up today!</h1>
 				<span class="md-subhead">Be among the teachers who are leading the movement toward a more transparent and accessible education model.</span>
 			</md-card-header>
-			<md-field>
+
+			<md-field :class="{'md-invalid': form.errors.has('firstname')}">
 				<md-icon>person</md-icon>
 				<label>Your first name:</label>
 				<md-input
-					v-model="firstname"
+					data-name="firstname"
+					v-model="form.firstname"
 					placeholder="Please enter your first name here."
 					maxlength="50"
 				></md-input>
+				<span 
+      				class="md-error"
+      				v-if="form.errors.has('firstname')"
+      				v-text="form.errors.get('firstname')"
+      			></span>
 			</md-field>
 
-			<md-field>
+			<md-field :class="{'md-invalid': form.errors.has('lastname')}">
 				<md-icon>person</md-icon>
 				<label>Your last name:</label>
 				<md-input
-					v-model="lastname"
+					data-name="lastname"
+					v-model="form.lastname"
 					placeholder="Please enter your last name here."
 					maxlength="50"
 				></md-input>
+				<span 
+      				class="md-error"
+      				v-if="form.errors.has('lastname')"
+      				v-text="form.errors.get('lastname')"
+      			></span>
 			</md-field>
 
-			<md-field>
+			<md-field :class="{'md-invalid': form.errors.has('email')}">
 				<md-icon>email</md-icon>
-				<label>Your Email:</label>
+				<label>Your email:</label>
 				<md-input
-					v-model="email"
-					placeholder="Please enter your email address here."
+					data-name="email"
+					v-model="form.email"
+					placeholder="Please enter your school email address here."
 				></md-input>
+				<span 
+      				class="md-error"
+      				v-if="form.errors.has('email')"
+      				v-text="form.errors.get('email')"
+      			></span>
 			</md-field>
 
-			<md-field>
+			<md-field :class="{'md-invalid': form.errors.has('password')}">
 				<md-icon>lock</md-icon>
 				<label>Your password:</label>
 				<md-input
+					data-name="password"
 					type="password"
-					v-model="password"
+					v-model="form.password"
 					placeholder="Enter a password at least 6 characters long."
 				></md-input>
+      			<span 
+      				class="md-error"
+      				v-if="form.errors.has('password')"
+      				v-text="form.errors.get('password')"
+      			></span>
 			</md-field>
 
 			<md-checkbox 
-				v-model="terms"
-			></md-checkbox>
-				By clicking this box you agree to the <a href="#">Terms and Conditions of Use</a> as well as our <a href="http://google.com" target="_blank">Privacy Policy</a>.
+				data-name="terms"
+				v-model="form.terms"
+				@change="form.errors.clear('terms')"
+			>
+				By clicking this box you agree to the <a href="#">Terms and Conditions of Use</a> as well as our <a href="#">Privacy Policy</a>.
+			</md-checkbox>
+			<span 
+  				:class="{'error': form.errors.has('terms')}"
+  				v-if="form.errors.has('terms')"
+  				v-text="form.errors.get('terms')"
+  			></span>
 
 			<md-card-actions>
-			<md-button class="md-primary md-raised space-below" :disabled="!formIsComplete" type="submit">Create an Account</md-button>	
+			<md-button class="md-primary md-raised space-below" :disabled="form.errors.any()" type="submit">Create an Account</md-button>	
 			</md-card-actions>
-		</form>
+		</SmartForm>
 	</md-card>
 </template>
 
 <script>
+	import Form from 'Js/lib/Form';
+	import Fetcher from 'Js/lib/Fetcher';
+	import SmartForm from 'Js/components/common/SmartForm';
+	import { submitProfRegistration, submitLogin } from 'Js/store/api';
+
 	export default {
 		name: 'TeacherRegistrationCard',
+		components: {
+			SmartForm,
+		},
 		data() {
 			return {
-				firstname: '',
-				lastname: '',
-				email: '',
-				password: '',
-				terms: false,
-				errors: [],
+				/**
+				 * The form object for registration
+				 * @type {Form}
+				 */
+				form: new Form({
+					firstname: '',
+					lastname: '',
+					email: '',
+					password: '',
+					terms: false,
+				}),
 			};
 		},
-		computed: {
-			formIsComplete() {
-				return this.firstname.length > 0 &&
-						this.lastname.length > 0 &&
-						this.email.length > 0 &&
-						this.password.length > 0 &&
-						this.terms == true &&
-						errors.length == 0;
-			}
-		},
 		methods: {
+			/**
+			 * Handle the submitting the form
+			 * @return {Void} 
+			 */
 			submitForm() {
-				console.log('registration form for teacher was submitted ');
-			}
+				const {query} = this.$route;
+				const target = query.redirect ? query.redirect : 'index';
+				const params = { ...query };
+				delete params.redirect;
+
+				this.form.submit(submitProfRegistration).then(() => {
+					this.$router.push({name: target, params});
+				});
+			},
 		},
 	};
 </script>
+
+<style scoped>
+	.error {
+		color: #ff1744;
+	}
+</style>
