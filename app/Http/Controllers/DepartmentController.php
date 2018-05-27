543529b28b18da435413f3a9e270619321ee466d
Verdi.K <verdi@victron-tech.com>
2018-05-27 02:16:04 -0500

    Complete the DepartmentController
    By finishing the methods I left empty earlier.

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\Models\School\Department;
 use Illuminate\Http\Request;
 use Optimus\Bruno\EloquentBuilderTrait;
@@ -39,14 +40,45 @@ class DepartmentController extends LaravelController
         return $dept;
     }
 
+    /**
+     * Handle a request to create a department after validation
+     * @param  \Illuminate\Http\Request $request Request
+     * @return \App\Models\School\Department           
+     */
     public function create(Request $request)
     {
-    	# code...
+        Validator::make($request->all(), [
+            'name' => 'required|max:255|unique:departments',
+        ])->validate();
+
+        return Department::create([
+            'name' => $request->name,
+         ]);
     }
 
+    /**
+     * Handle the request to update a department after validation
+     * @param  \App\Models\School\Department $dept    The department entry to update
+     * @param  \Illuminate\Http\Request    $request Request
+     * @return void              
+     */
     public function update(Department $dept, Request $request)
     {
-    	# code...
+        Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ])->validate();
+
+        $isNameTaken = Department::where('name', $request->name)
+            ->whereNotIn('id', $dept->id)
+            ->first();
+
+        if($isNameTaken) {
+            $errors = ['name' => ['This name is already used.']];
+            return response()->json(compact('errors'), 422);
+        }
+
+        $dept->name = $request->name;
+        $dept->save();
     }
 
     /**
