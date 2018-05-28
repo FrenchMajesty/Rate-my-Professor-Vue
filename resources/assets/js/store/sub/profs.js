c870f2fc6f547a59139d0b08cfdbd16fd68ba637
Verdi.K <verdi@victron-tech.com>
2018-05-27 19:20:09 -0500

    Add getters to profs branch of store
    I realized that many operations that involved the store were being done multiple times (for example the prof page and page to rate said prof) so the best decision to make my life easier is to create a getter to do all that tedious stuff for me.

--- resources/assets/js/store/sub/profs.js
+++ resources/assets/js/store/sub/profs.js
@@ -9,6 +9,46 @@ const profs = {
 		profDataIsFetching(state) {
 			return state.prof.beingFetched;
 		},
+
+		/**
+		 * Determine whether professors and all aggregate data is ready for use
+		 * @param  {Object} state The store's state
+		 * @return {Boolean}       
+		 */
+		profDataIsReady(state) {
+			return !(state.prof.beingFetched || state.school.beingFetched || state.dept.beingFetched);
+		},
+
+		/**
+		 * Return a function to find the full entry of a professor
+		 * @param  {Object} state The store's state
+		 * @return {Function}       
+		 */
+		fullProfessor(state) {
+			return (profId) => {
+				const prof = state.prof.data.find(({id}) => id == profId);
+
+				if(prof) {
+					prof.school = state.school.data.find(({id}) => id == prof.school_id);
+					prof.department = state.dept.data.find(({id}) => id == prof.department_id);
+				}
+
+				return prof ? prof : null;
+			};
+		},
+
+		/**
+		 * Find a professor with the slug
+		 * @param  {Object} state   The store's state
+		 * @param  {Object} getters The store's getters
+		 * @return {Function}         
+		 */
+		fullProfessorWithSlug(state, getters) {
+			return (slug) => {
+				const {id} = state.prof.data.find((prof) => prof.slug == slug);
+				return id ? getters.fullProfessor(id) : null;
+			}
+		}
 	},
 	mutations: {
 		/**
