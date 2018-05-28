f15361b563c7eb8269fb7d6a1126ee08de00924f
Verdi.K <verdi@victron-tech.com>
2018-05-28 10:18:05 -0500

    Add `fullSchool` getters to find a school with the ID or slug

--- resources/assets/js/store/sub/schools.js
+++ resources/assets/js/store/sub/schools.js
@@ -1,20 +1,53 @@
 const schools = {
 	state: {
 		school: {
-			data: null,
+			data: [],
 			beingFetched: false,
 		}
 	},
 	getters: {
+		/**
+		 * Determine whether schools and all aggregate data is ready for use
+		 * @param  {Object} state The store's state
+		 * @return {Boolean}       
+		 */
+		schoolDataIsReady(state) {
+			return !(state.school.beingFetched || state.dept.beingFetched);
+		},
+
 		/**
 		 * Filter and return only schools that have been approved
 		 * @param  {Object} state.school The school branch of the app's state
 		 * @return {Array}       
 		 */
 		approvedSchools({school}) {
-			if(school.data && school.data.length > 0) {
-				return school.data.filter(school => school.approved);
-			}
+			return school.data.filter(school => school.approved);
+		},
+
+		/**
+		 * Return a function to find the full entry of a school
+		 * @param  {Object} state The store's state
+		 * @param  {Object} getters The store's getters
+		 * @return {Function}       
+		 */
+		fullSchool(state, getters) {
+			return (schoolId) => {
+				const school = getters.approvedSchools.find(({id}) => id == schoolId);
+				return school;
+			};
+		},
+
+		/**
+		 * Find a school entry with the slug
+		 * @param  {Object} state   The store's state
+		 * @param  {Object} getters The store's getters
+		 * @return {Function}         
+		 */
+		fullSchoolWithSlug(state, getters) {
+			return (slug) => {
+				const school = getters.approvedSchools.find(school => school.slug == slug);
+				return school ? getters.fullSchool(school.id) : null;
+			};
 		},
 	},
 	mutations: {
