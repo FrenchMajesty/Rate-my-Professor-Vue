e858d5434dc5ce462c37fa4e720a1d5bd5767a6d
Verdi.K <verdi@victron-tech.com>
2018-05-28 00:33:16 -0500

    Make RateProfessor page functional

--- resources/assets/js/views/RateProfessor.vue
+++ resources/assets/js/views/RateProfessor.vue
@@ -1,10 +1,13 @@
 <template>
 	<div class="md-layout md-gutter no-margin md-alignment-top-space-around">
-		<md-card class="md-layout-item md-xsmall-size-90 md-small-size-80 md-medium-size-45 md-size-40 vertical-spacing" md-with-hover>
-			<md-card-header>
+		<md-card 
+			class="md-layout-item md-xsmall-size-90 md-small-size-80 md-medium-size-45 md-size-40 vertical-spacing" 
+			md-with-hover
+		>
+			<md-card-header v-if="professor" class="animated fadeIn">
 				<md-card-header-text>
 					<div class="md-title">{{professor.name}}</div>
-					<div class="md-subhead">Professor of @... at {{professor.school.name}}</div>
+					<div class="md-subhead">Professor of {{professor.department.name}} at {{professor.school.name}}</div>
 				</md-card-header-text>
 
 				<md-card-media md-medium>
@@ -17,44 +20,55 @@
 	            	<md-icon>share</md-icon>
       				<md-tooltip md-direction="top">Share this professor's page</md-tooltip>
 	          	</md-button>
-				<md-button :to="{name: 'professor', params:{slug: professor.slug}}">Open profile page</md-button>
+				<md-button 
+					v-if="professor"
+					:to="{name: 'professor', params:{slug: professor.slug}}"
+				>Open profile page</md-button>
 			</md-card-actions>
 		</md-card>
 		<md-card class="md-layout-item md-xsmall-size-90 md-small-size-80 md-medium-size-45 md-size-50">
 			<SmartForm :submit="submitForm" :form="form">
 				<md-card-header>
 					<h1 class="md-title">Rate your professor</h1>
-					<span class="md-subhead">It's your turn to grade Professor {{professor.name}}! Make sure to give an accurate review, the fate of future students is in your hands.</span>
+					<span class="md-subhead" v-if="professor">It's your turn to grade Professor {{professor.name}}! Make sure to give an accurate review, the fate of future students is in your hands.</span>
 				</md-card-header>
 
-				<md-switch class="switcher" v-model="form.textbook">Was the textbook used?</md-switch>
+				<md-switch
+					class="switcher"
+					v-model="form.textbook_used"
+					@change="form.errors.clear('textbook_used')"
+				>Was the textbook used?</md-switch>
 				<p 
 					class="text-red"
-      				v-if="form.errors.has('textbook')"
-      				v-text="form.errors.get('textbook')"
+      				v-if="form.errors.has('textbook_used')"
+      				v-text="form.errors.get('textbook_used')"
 				></p>
 
-				<md-switch class="switcher" v-model="form.retake">Would you re-take this prof again?</md-switch>
+				<md-switch 
+					class="switcher"
+					v-model="form.would_retake"
+					@change="form.errors.clear('would_retake')"
+					>Would you re-take this prof again?</md-switch>
 				<p 
 					class="text-red"
-      				v-if="form.errors.has('retake')"
-      				v-text="form.errors.get('retake')"
+      				v-if="form.errors.has('would_retake')"
+      				v-text="form.errors.get('would_retake')"
 				></p>
 
-				<md-field :class="{'md-invalid': form.errors.has('classcode')}">
+				<md-field :class="{'md-invalid': form.errors.has('class')}">
 					<md-icon>school</md-icon>
 					<label>Your class code:</label>
 					<md-input
-						data-name="classcode"
-						:value="form.classcode.toUpperCase().replace(/\s/,'')" 
-						@input="form.classcode = $event"
+						data-name="class"
+						:value="form.class.toUpperCase().replace(/\s/,'')" 
+						@input="form.class = $event"
 						placeholder="Please enter your class code here."
 						:maxlength="10"
 					></md-input>
 					<span 
 	      				class="md-error"
-	      				v-if="form.errors.has('classcode')"
-	      				v-text="form.errors.get('classcode')"
+	      				v-if="form.errors.has('class')"
+	      				v-text="form.errors.get('class')"
 	      			></span>
 	      			<span 
 	      				class="md-helper-text" 
@@ -64,9 +78,14 @@
 	      			</span>
 				</md-field>
 
-				<md-field :class="{'md-invalid': form.errors.has('grade')}">
+				<md-field :class="{'md-invalid': form.errors.has('grade_received')}">
 			    	<label for="grade">The grade you received:</label>
-			        <md-select v-model="form.grade" name="grade" id="grade">
+			        <md-select 
+			        	v-model="form.grade_received"
+			        	name="grade"
+			        	id="grade"
+			        	@md-opened="form.errors.clear('grade_received')"
+			        >
 			        	<md-option 
 			        		v-for="(grade, i) in grades"
 			        		:key="i"
@@ -75,38 +94,50 @@
 			        </md-select>
 					<span 
 	      				class="md-error"
-	      				v-if="form.errors.has('grade')"
-	      				v-text="form.errors.get('grade')"
+	      				v-if="form.errors.has('grade_received')"
+	      				v-text="form.errors.get('grade_received')"
 	      			></span>
 				</md-field>
 
-				<label>Select up to 3 tags that best describe your professor:</label>
+				<label>Select up to {{tagsLimit}} tags that best describe your professor:</label>
 				<br/>
 				<md-chip 
+					v-if="tags"
 					v-for="(tag, i) in tags" 
 					:key="tag.id" 
 					:data-id="tag.id"
     				:class="generateTagClass(tag.id)" 
     				@click="toggleTag"
     				style="margin-top: 1em"
-    			>{{tag.value}}</md-chip>
+    			>{{tag.name}}</md-chip>
+    			<p 
+					class="text-red"
+      				v-if="form.errors.has('tags_id')"
+      				v-text="form.errors.get('tags_id')"
+				></p>
 
 				<md-field :class="{'md-invalid': form.errors.has('comment')}">
 			      	<label>Write your review:</label>
 			     	<md-textarea 
+			     		data-name="comment"
 			     		v-model="form.comment"
 			     		placeholder="You can press Enter to go to the next line."
 			     		:maxlength="350"
 			      		md-autogrow
 			    	></md-textarea>
 			    	<md-icon>description</md-icon>
+			    	<span 
+	      				class="md-error"
+	      				v-if="form.errors.has('comment')"
+	      				v-text="form.errors.get('comment')"
+	      			></span>
 			    </md-field>
 
-			    <p>By clicking the 'Submit' button, I acknowledge that I have read and agreed to the <a href="#">Site Guidelines</a>, <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.</p>
+			    <p>By clicking the submit button, I acknowledge that I have read and agreed to the <a href="#">Site Guidelines</a>, <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.</p>
 
 	    		<md-progress-bar v-if="isSubmitting" md-mode="query"></md-progress-bar>
 				<md-card-actions>
-				<md-button :disabled="form.errors.any()" type="submit">Submit my review</md-button>
+					<md-button :disabled="form.errors.any()" type="submit">Submit my review</md-button>
 				</md-card-actions>
 			</SmartForm>
 		</md-card>
@@ -117,6 +148,7 @@
 	import Form from 'Js/lib/Form';
 	import Fetcher from 'Js/lib/Fetcher';
 	import SmartForm from 'Js/components/common/SmartForm';
+	import { createProfessorReview } from 'Js/store/api';
 
 	export default {
 		name: 'RateProfessor',
@@ -128,8 +160,9 @@
 		 * Fetch all the professors and schools datas
 		 */
 		beforeCreate() {
-			Fetcher.profs(this).schools(this);
+			Fetcher.all(this);
 		},
+
 		/**
 		 * Get the quick review from the store's state and reset its value
 		 */
@@ -146,13 +179,15 @@
 				 * @type {Form}
 				 */
 				form: new Form({
-					overall: 0,
-					difficulty: 0,
-					retake: false,
-					textbook: false,
-					grade: '',
-					classcode: '',
+					professor_id: this.$route.params.id,
+					overall_rating: '',
+					difficulty_rating: '',
+					would_retake: false,
+					textbook_used: false,
+					grade_received: '',
+					class: '',
 					comment: '',
+					tags_id: '',
 				}),
 
 				/**
@@ -172,17 +207,22 @@
 				 * @type {Array}
 				 */
 				grades: ['A+','A','A-','B+','B','B-','C+','C','C-','D+','D','D-','F','Dropped','Not sure yet','Rather not say'],
+
+				/**
+				 * The maximum number of tags allowed per review
+				 * @type {Number}
+				 */
+				tagsLimit: 3,
 			}
 		},
 		computed: {
 
 			/**
-			 * Determine whether the professors' datas are ready to be used
+			 * Determine whether the datas are ready to be used
 			 * @return {Boolean} 
 			 */
 			dataIsReady() {
-				const {beingFetched, data} = this.$store.state.prof;
-				return ! beingFetched && data !== null;
+				return this.$store.getters.profDataIsReady;
 			},
 			/**
 			 * Get the professor for whom this review is being written for
@@ -190,31 +230,19 @@
 			 */
 			professor() {
 				const {id} = this.$route.params;
-				const {data} = this.$store.state.prof;
-				const {data: schools} = this.$store.state.school;
+				const {fullProfessor} = this.$store.getters;
 
-				if(data && schools) {
-					const prof = data.find((prof) => prof.id == id);
-					prof.school = schools.find(({id}) => id == prof.school_id);
-
-					return prof;
+				if(this.dataIsReady) {
+					return fullProfessor(id);
 				}
 			},
 
 			/**
-			 * Get the professor's tags to describe them
+			 * Get the review tags to describe the review
 			 * @return {Array} 
 			 */
 			tags() {
-				const tags = [
-					{id: 1, value: 'Respected'},
-					{id: 2, value: 'Get Ready to Read'},
-					{id: 3, value: 'Lots of Homework'},
-					{id: 4, value: 'Hilarious'},
-					{id: 5, value: 'Test Heavy'},
-				];
-
-				return tags;
+				return this.$store.state.tags.data;
 			},
 		},
 		watch: {
@@ -229,6 +257,15 @@
 					this.$router.push({name: 'notFound'});
 				}
 			},
+
+			/**
+			 * Keep the `selectedTagsId` array and form.tags_id synchronized
+			 * @param  {Array} tagsId The array of the tags selected
+			 * @return {Void}        
+			 */
+			selectedTagsId(tagsId) {
+				this.form.tags_id = tagsId.join(',');
+			}
 		},
 		methods: {
 			/**
@@ -252,13 +289,14 @@
 			toggleTag({target}) {
 				const id = Number(target.getAttribute('data-id'));
 				const isSelected = this.selectedTagsId.some(tagId => tagId == id);
+				this.form.errors.clear('tags_id');
 
 				if(isSelected) {
 					this.selectedTagsId = this.selectedTagsId.filter(tagId => tagId != id);
 					return;
 				}
 
-				if(this.selectedTagsId.length < 3) {
+				if(this.selectedTagsId.length < this.tagsLimit) {
 					this.selectedTagsId.push(id);
 				}
 			},
@@ -268,7 +306,14 @@
 			 * @return {Void} 
 			 */
 			submitForm() {
-				console.log('form submitted!');
+				const success = () => {
+					alert('Your review was successfully sent!');
+
+					const params = { slug: this.professor.slug };
+					this.$router.push({name: 'professor', params});
+				};
+
+				this.form.submit(createProfessorReview).then(success);
 			},
 		}
 	}
