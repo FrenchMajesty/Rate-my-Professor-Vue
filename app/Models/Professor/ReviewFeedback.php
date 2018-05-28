99139e42693e2d4cc49aef73e420f47e5c3e13bf
Verdi.K <verdi@victron-tech.com>
2018-05-28 16:35:47 -0500

    Correct error 'table not found' in ReviewFeedback model
    The table name is not the one generated by default by Laravel.

--- app/Models/Professor/ReviewFeedback.php
+++ app/Models/Professor/ReviewFeedback.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class ReviewFeedback extends Model
 {
 	/**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'professor_review_feedbacks';
+
+	/**
 	 * The attributes that are mass assignable
 	 * @var array
 	 */
