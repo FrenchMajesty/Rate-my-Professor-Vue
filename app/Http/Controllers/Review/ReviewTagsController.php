75b8d714cc13c811e4aa96f111fed46f892ed7cf
Verdi.K <verdi@victron-tech.com>
2018-05-28 16:34:07 -0500

    Correct error 'Could not find Controller' on ReviewsTagsController

--- app/Http/Controllers/Review/ReviewTagsController.php
+++ app/Http/Controllers/Review/ReviewTagsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Review;
 
 use App\Models\Professor\ReviewTags;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ReviewTagsController extends Controller
 {	
