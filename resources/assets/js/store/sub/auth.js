0bbc61c9e774a5444f9dc93599f7eaef93bfa85e
Verdi.K <verdi@victron-tech.com>
2018-05-28 19:38:35 -0500

    Add IP branch to the store/auth
    I need to store a user's IP address in the store's state.

--- resources/assets/js/store/sub/auth.js
+++ resources/assets/js/store/sub/auth.js
@@ -5,6 +5,11 @@ const auth = {
 			accessToken: null,
 			expire: null,
 		},
+		ip: {
+			beingFetched: false,
+			lastUpdated: null,
+			ipAddress: '',
+		},
 	},
 	getters: {
 
@@ -12,7 +17,7 @@ const auth = {
 	mutations: {
 		/**
 		 * Update the access token in the state
-		 * @param  {Object} state          The app's state
+		 * @param  {Object} state          The store's state
 		 * @param  {String} payload.token  The new access token
 		 * @param  {Number} payload.expire The time until it expires in seconds
 		 * @return {Void}                
@@ -24,13 +29,23 @@ const auth = {
 
 		/**
 		 * Update the user model in the state if existing or set it
-		 * @param  {Object} state        The app's state
+		 * @param  {Object} state        The store's state
 		 * @param  {Object} payload.user The new user model
 		 * @return {Void}              
 		 */
 		updateUser(state, {user}) {
 			state.user = state.user ? { ...state.user, ...user } : user;
 		},
+
+		/**
+		 * Update the IP branch in the state with the given data
+		 * @param  {Object} state      The store's state
+		 * @param  {Object} payload The properties of IP to update 
+		 * @return {Void}            
+		 */
+		updateIpAddress(state, payload) {
+			state.ip = { ...state.ip, ...payload };
+		}
 	},
 };
 
